c732ad9dc5e71376d52a1a2a735041319e410b66
vyadav <vyadav@C5-VINOD.ad.utb.cz>
2021-03-22 14:30:55 +0100

    With first Project

---
+++ StudentCrud/src/main/java/com/example/StudentCrud/domain/Student.java
@@ -0,0 +1,53 @@
+package com.example.StudentCrud.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	private String studentname;
+	private String course;
+	private int fee;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getStudentname() {
+		return studentname;
+	}
+	public void setStudentname(String studentname) {
+		this.studentname = studentname;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	public int getFee() {
+		return fee;
+	}
+	public void setFee(int fee) {
+		this.fee = fee;
+	}
+	public Student(long id, String studentname, String course, int fee) {
+		super();
+		this.id = id;
+		this.studentname = studentname;
+		this.course = course;
+		this.fee = fee;
+	}
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+}
